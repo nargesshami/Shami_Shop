ab9c2fd301f18eec3556545b7d081ca143cee061
nargessgami <nargesshamii81@gmail.com>
2023-05-18 15:50:24 +0430

    Create user model

--- Shami_Shop/Models/Users.cs
+++ Shami_Shop/Models/Users.cs
@@ -1,7 +1,15 @@
-﻿namespace Shami_Shop.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shami_Shop.Models
 {
     public class Users
     {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        [MaxLength(11)]
+        public string MobilePhone { get; set; }
+        public bool IsAdmin { get; set; }
 
     }
 }
